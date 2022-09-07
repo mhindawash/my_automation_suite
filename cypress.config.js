07c51990257cadf1844678a52f1d38b2abd29982
mohammad hindawash <djslakivich@mohammads-MacBook-Air.local>
2022-09-07 11:43:43 -0400

    First commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'q3n7h7',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
